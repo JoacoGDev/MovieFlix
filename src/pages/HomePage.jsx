862c3a7ce9879674866dc555436d5e76fc91d63b
JoacoGDev <joacogilp@gmail.com>
2026-06-03 18:32:26 -0300

    Feat: shows either carousels or grid if there is a query

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,36 @@
-import { getTrending, getPopularMovies, getPopularShows, getTopRatedMovies } from "../services/tmdb";
+import { getTrending, getPopularMovies, getPopularShows, getTopRatedMovies, searchMulti } from "../services/tmdb";
 import Carousel from "../components/Carousel";
+import { useEffect, useCallback } from "react";
+import { useSearchParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Grid from "../components/Grid";
+
 
 function HomePage() {
+
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("query") || "";
+
+    const searchFunction = useCallback(query ? () => searchMulti(query) : null, [query]);
+
+    const { data, isLoading, error } = useFetch(searchFunction);
 
     const carouselData = [{ title: "Trending Now", fetch: getTrending },
     { title: "Popular Movies", fetch: getPopularMovies },
     { title: "Popular Shows", fetch: getPopularShows },
     { title: "Top Rated Movies", fetch: getTopRatedMovies }];
 
+
     return (
         <div className="min-h-screen bg-zinc-950 px-6 pt-25 pb-8">
 
-            {carouselData.map((carousel) => (
+
+
+            {query.length === 0 ? carouselData.map((carousel) => (
                 <Carousel key={carousel.title} title={carousel.title} fetchFunction={carousel.fetch} />
-            ))}
+            )) : query && data &&
+            <Grid title={`Results for ${query}`} data={data.results} />
+            }
         </div>
 
     );
